bdc0888d351e81a2af1fab6a9f173338de4d6db0
Janetrix <janetrix.gicangiru@student.moringaschool.com>
2025-07-28 20:42:14 +0300

    tailwind

--- cart/vite-project/tailwind.config.js
+++ cart/vite-project/tailwind.config.js
@@ -1,10 +1,17 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'electric-purple': '#8a2be2',
+        'electric-purple-light': '#b57edc',
+        'electric-purple-dark': '#5a1e9e',
+      }
+    },
   },
   plugins: [],
 }
