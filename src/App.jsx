2b01ba73db110544e25a7d50b619c590e246944a
Janetrix576 <janetrix.gicangiru@student.moringaschool.com>
2025-07-29 09:46:47 +0300

    Ft checkout (#12)
    * checkout
    
    * checkout
    
    * checkout
    
    * checkout
    
    * checkout
    
    * checkout
    
    * Regenerate package-lock.json to resolve merge conflicts
    
    ---------
    
    Co-authored-by: Eugene <eugene.chege@student.moringaschool.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-<<<<
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import ProductListPage from './pages/ProductListPage';
 import ProductDetailPage from './pages/ProductDetailPage';
-
-function App() {
-  return (
- 
-      <div className="min-h-screen bg-purple-200 text-black">
-        <Navbar />
-        <div className="max-w-6xl mx-auto py-8">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductListPage />} />
-            <Route path="/products/:slug" element={<ProductDetailPage />} />
-          </Routes>
-        </div>
-      </div>
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CheckoutPage from './pages/CheckoutPage';
 import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
+      <div className="min-h-screen bg-white text-black dark:text-white">
+        <Navbar />
+        <div className="max-w-6xl mx-auto py-8">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/products" element={<ProductListPage />} />
+            <Route path="/products/:slug" element={<ProductDetailPage />} />
+            <Route path="/checkoutpage" element={<CheckoutPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
-
   );
 }
 
