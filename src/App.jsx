6c0ead1d386885f4c64f24cb3e78d43f5875fd63
Allan Kinuva <allankinuva@gmail.com>
2025-07-29 11:36:22 +0300

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
+  const clientId = '642335350301-goq2co22qho68mrb548lu7jto62kief5.apps.googleusercontent.com'; 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </Router>
+    <GoogleOAuthProvider clientId={clientId}>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
