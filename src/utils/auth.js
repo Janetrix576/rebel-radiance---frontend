ac283d23c72cc53102d7483e07b5317d47f9d184
Stean <stean.ngugi@student.moringaschool.com>
2025-08-01 10:04:29 +0300

    Final commit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://rebel-radiance-backend.onrender.com/api/', 
+  baseURL: 'https://rebel-radiance-backend.onrender.com/api/',
 });
 
 api.interceptors.request.use(
